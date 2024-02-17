a487604e5979ca6ac31f609488eb57b942d7eae7
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-17 13:50:50 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,46 @@
+let a = prompt();
+let p = "HELLO";
+
+let name1 = "Name : ";
+let name = "My name is khushboo";
+
+let code = "My bage code is 2310f";
+let bcode = "Code : ";
+let c = "Thankyou";
+
+let email = "khushboojamali6@email.com";
+let emailu = "Email : ";
+
+let contact = 3003252919;
+let contactnum = " Contact : ";
+let t = true;
+let tf = " True/False  : ";
+
+let money = "Rs :";
+let number = 190;
+let numbers = 80;
+console.log(p);
+console.log(name1, name);
+console.log(bcode, code);
+console.log(emailu, email);
+console.log(contactnum + contact);
+console.log(tf, t);
+console.log(money, number + numbers);
+
+document.write(p);
+document.write("<br><br>", name1, name);
+document.write("<br><br>", bcode, code);
+document.write("<br> <br>", emailu, email);
+document.write("<br><br>", contactnum + contact);
+document.write("<br> <br>", tf, t);
+document.write("<br> <br>", money, number + numbers);
+
+// console.log("*Hello Sir*");
+// console.log(name1, name);
+// console.log(bcode, code);
+
+// console.log(emailu, email);
+// console.log(contactnum, contact);
+// console.log(tf, t);
+// console.log(money, number + numbers);
+// console.log(c);
